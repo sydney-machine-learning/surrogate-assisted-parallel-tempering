657a20d4a65e248bf7527e744259e6a14ac7796f
Rohitash Chandra <c.rohitash@gmail.com>
2018-11-16 11:36:57 +1100

    results updated

--- run_langevin.sh
+++ run_langevin.sh
@@ -2,20 +2,32 @@
 echo Running all 	 
  
 
-python surrogate_pt_classifier_langevingrad.py 3 50000   0.5  0.05  surrlangevingrad/
+#python surrogate_pt_classifier_langevingrad.py 3 50000   0.5  0.05  surrlangevingrad/ 
+#python surrogate_pt_classifier_langevingrad.py 3 50000   0.5  0.05  surrlangevingrad/ 
+#python surrogate_pt_classifier_langevingrad.py 4 50000   0.5  0.05  surrlangevingrad/
+#python surrogate_pt_classifier_langevingrad.py 4 50000   0.5  0.05  surrlangevingrad/
+
+#python surrogate_pt_classifier_langevingrad.py 5 50000   0.5  0.05  surrlangevingrad/
+#python surrogate_pt_classifier_langevingrad.py 5 50000   0.5  0.05  surrlangevingrad/
 
 
 
-python surrogate_pt_classifier_langevingrad.py 3 50000   0.5  0.05  surrlangevingrad/ 
-python surrogate_pt_classifier_langevingrad.py 4 50000   0.5  0.05  surrlangevingrad/
-python surrogate_pt_classifier_langevingrad.py 4 50000   0.5  0.05  surrlangevingrad/
+#python surrogate_pt_classifier_langevingrad.py 3 50000   0.25  0.05  surrlangevingrad/ 
+#python surrogate_pt_classifier_langevingrad.py 3 50000   0.25  0.05  surrlangevingrad/ 
+#python surrogate_pt_classifier_langevingrad.py 4 50000   0.25  0.05  surrlangevingrad/
+#python surrogate_pt_classifier_langevingrad.py 4 50000   0.25  0.05  surrlangevingrad/
 
-python surrogate_pt_classifier_langevingrad.py 5 50000   0.5  0.05  surrlangevingrad/
-python surrogate_pt_classifier_langevingrad.py 5 50000   0.5  0.05  surrlangevingrad/
+#python surrogate_pt_classifier_langevingrad.py 5 50000   0.25  0.05  surrlangevingrad/
+#python surrogate_pt_classifier_langevingrad.py 5 50000   0.25  0.05  surrlangevingrad/
+
+
 python surrogate_pt_classifier_langevingrad.py 6 50000 0.5 0.05 surrlangevingrad/ 
+python surrogate_pt_classifier_langevingrad.py 6 50000   0.25  0.05  surrlangevingrad/
+
 python surrogate_pt_classifier_langevingrad.py 7 50000   0.5  0.05  surrlangevingrad/
+python surrogate_pt_classifier_langevingrad.py 7 50000 0.25 0.05 surrlangevingrad/  
 python surrogate_pt_classifier_langevingrad.py 8 50000   0.5  0.05  surrlangevingrad/
-#python surrogate_pt_classifier_langevingrad.py 8 50000 0.25 0.05 surrlangevingrad/  
+python surrogate_pt_classifier_langevingrad.py 8 50000 0.25 0.05 surrlangevingrad/  
 
 #python surrogate_pt_classifier_langevingrad.py 3 50000   0.25  0.05  surrlangevingrad/ 
 #python surrogate_pt_classifier_langevingrad.py 4 50000   0.25  0.05  surrlangevingrad/
