c4818606fdcc1b7bbd168216815a96408c3fb50f
RC <c.rohitash@gmail.com>
2020-03-03 14:17:54 +1100

    removing not needed results - updated gitignore

--- run_probability_commoninterval.sh
+++ run_probability_commoninterval.sh
@@ -5,24 +5,26 @@ echo Running all
  
 
  
-python surrogate_pt_classifier_rw_common_interval.py 5 50000   0.5  0.05  num_replicas_rw/ 4
-python surrogate_pt_classifier_rw_common_interval.py 5 50000   0.5  0.05  num_replicas_rw/ 6
-python surrogate_pt_classifier_rw_common_interval.py 5 50000   0.5  0.05  num_replicas_rw/ 8 
-python surrogate_pt_classifier_rw_common_interval.py 5 50000   0.5  0.05  num_replicas_rw/ 10
+#python surrogate_pt_classifier_rw_common_interval.py 5 50000   0.5  0.05  num_replicas_rw/ 4
+#python surrogate_pt_classifier_rw_common_interval.py 5 50000   0.5  0.05  num_replicas_rw/ 6
+#python surrogate_pt_classifier_rw_common_interval.py 5 50000   0.5  0.05  num_replicas_rw/ 8 
+#python surrogate_pt_classifier_rw_common_interval.py 5 50000   0.5  0.05  num_replicas_rw/ 10
+
+python surrogate_pt_classifier_rw_common_interval_mod_ashray.py 5 10000   0.5  0.05  num_replicas_rw/ 10
  
-python surrogate_pt_classifier_rw_common_interval.py 6 50000   0.5  0.05  num_replicas_rw/ 4
-python surrogate_pt_classifier_rw_common_interval.py 6 50000   0.5  0.05  num_replicas_rw/ 6
-python surrogate_pt_classifier_rw_common_interval.py 6 50000   0.5  0.05  num_replicas_rw/ 8 
-python surrogate_pt_classifier_rw_common_interval.py 6 50000   0.5  0.05  num_replicas_rw/ 10
+#python surrogate_pt_classifier_rw_common_interval.py 6 50000   0.5  0.05  num_replicas_rw/ 4
+#python surrogate_pt_classifier_rw_common_interval.py 6 50000   0.5  0.05  num_replicas_rw/ 6
+#python surrogate_pt_classifier_rw_common_interval.py 6 50000   0.5  0.05  num_replicas_rw/ 8 
+#python surrogate_pt_classifier_rw_common_interval.py 6 50000   0.5  0.05  num_replicas_rw/ 10
 
  
-python surrogate_pt_classifier_rw_common_interval.py 7 50000   0.5  0.05  num_replicas_rw/ 4
-python surrogate_pt_classifier_rw_common_interval.py 7 50000   0.5  0.05  num_replicas_rw/ 6
-python surrogate_pt_classifier_rw_common_interval.py 7 50000   0.5  0.05  num_replicas_rw/ 8 
-python surrogate_pt_classifier_rw_common_interval.py 7 50000   0.5  0.05  num_replicas_rw/ 10
+#python surrogate_pt_classifier_rw_common_interval.py 7 50000   0.5  0.05  num_replicas_rw/ 4
+#python surrogate_pt_classifier_rw_common_interval.py 7 50000   0.5  0.05  num_replicas_rw/ 6
+#python surrogate_pt_classifier_rw_common_interval.py 7 50000   0.5  0.05  num_replicas_rw/ 8 
+#python surrogate_pt_classifier_rw_common_interval.py 7 50000   0.5  0.05  num_replicas_rw/ 10
  
-python surrogate_pt_classifier_rw_common_interval.py 8 50000   0.5  0.05  num_replicas_rw/ 4
-python surrogate_pt_classifier_rw_common_interval.py 8 50000   0.5  0.05  num_replicas_rw/ 6
-python surrogate_pt_classifier_rw_common_interval.py 8 50000   0.5  0.05  num_replicas_rw/ 8 
-python surrogate_pt_classifier_rw_common_interval.py 8 50000   0.5  0.05  num_replicas_rw/ 10
+#python surrogate_pt_classifier_rw_common_interval.py 8 50000   0.5  0.05  num_replicas_rw/ 4
+#python surrogate_pt_classifier_rw_common_interval.py 8 50000   0.5  0.05  num_replicas_rw/ 6
+#python surrogate_pt_classifier_rw_common_interval.py 8 50000   0.5  0.05  num_replicas_rw/ 8 
+#python surrogate_pt_classifier_rw_common_interval.py 8 50000   0.5  0.05  num_replicas_rw/ 10
  
