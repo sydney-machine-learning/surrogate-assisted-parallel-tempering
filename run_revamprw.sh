72ddc44e4452aaf6e1fdd63330d94bc8fabe1494
RC <c.rohitash@gmail.com>
2020-04-23 22:53:21 +1000

    running exp

--- run_revamprw.sh
+++ run_revamprw.sh
@@ -5,15 +5,31 @@ echo Running all
   
 max_samples=50000
 replicas=10
-surrogate_prob=0.5
-surrogate_interval=0.02
+#surrogate_prob=0.5
+#surrogate_interval=0.05
  
-python surrogate_revamp.py 4 $max_samples  $surrogate_prob  $surrogate_interval revamp_rw/ $replicas 
- 
-python surrogate_revamp.py 5 $max_samples  $surrogate_prob  $surrogate_interval revamp_rw/ $replicas   
- 
-python surrogate_revamp.py 6 $max_samples  $surrogate_prob  $surrogate_interval  revamp_rw/ $replicas  
- 
-python surrogate_revamp.py 7 $max_samples  $surrogate_prob   $surrogate_interval  revamp_rw/ $replicas   
+#python surrogate_revamp.py 4 $max_samples  $surrogate_prob  $surrogate_interval revamp_rwexp/ $replicas    
 
-python surrogate_revamp.py 8 $max_samples  $surrogate_prob  $surrogate_interval  revamp_rw/ $replicas 
+
+
+for problem in  4 5 6 7 8
+ 	do
+	for surrogate_interval in  0.05 #0.05 #0.02 0.05 0.10 0.1
+  		do
+		for surrogate_prob  in  0.6 # 0.2  0.4 0.6 0.8   #0.2 0.4 0.6 0.8 
+			do   
+
+
+			python _revamp.py $problem $max_samples  $surrogate_prob  $surrogate_interval revamp_rwexp/ $replicas   
+  
+  
+	done  
+ 
+  
+		
+
+		
+
+		
+	done
+done
